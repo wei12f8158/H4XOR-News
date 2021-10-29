306019aeaa97869d6f76b67614bdfd64fcd67a93
WEI <wei12f8158@gmail.com>
2021-10-29 10:28:22 +0800

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by 吳偉平 on 2021/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
